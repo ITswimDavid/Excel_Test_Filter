bb6784b1b80f64ee1e2434054eac4328de3aa42e
Simon Wischnevetski <simon.wischnevetski@gmail.com>
2023-06-16 22:25:37 +0200

    BASIC IMG xlsx

--- basic_read_img.py
+++ basic_read_img.py
@@ -1,37 +1,41 @@
+import os
+
 from openpyxl import load_workbook
 from PIL import Image
 from io import BytesIO
 import matplotlib.pyplot as plt
 from openpyxl.cell import Cell
+from openpyxl.workbook import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
 
 from openpyxl_image_loader import SheetImageLoader
 
 # Load the workbook and select the worksheet
 wb = load_workbook('your_file.xlsx')
-ws = wb.active
+wb: Workbook
+
+for sheet in wb.worksheets:
+    print(sheet.title)
+    sheet: Worksheet
+
+    print(type(sheet))
+
+ws = wb['Tabellle1']
 
 # calling the image_loader
 image_loader = SheetImageLoader(ws)
-
 
 # get the image (put the cell you need instead of 'A1')
 
 
 # showing the image
 # image.show()
-def is_image_in_cell(cell):
-    for image in ws._images:
-        anchor = image.anchor
-        if anchor.from_col <= cell.column <= anchor.to_col and anchor.from_row <= cell.row <= anchor.to_row:
-            return True
-
-    return False
 
 
 for i in range(1, ws.max_row + 1):
     row = [cell for cell in ws[i]]  # sheet[n] gives nth row (list of cells)
 
-    for cel in row:
+    for idx, cel in enumerate(row):
         cel: Cell  # use type hinting to get code completion
 
         cel_coordinate = cel.coordinate
@@ -39,11 +43,17 @@ for i in range(1, ws.max_row + 1):
             print("Cell: " + cel_coordinate)
             image_loader.image_in(cel_coordinate)
             image = image_loader.get(cel_coordinate)
-            image.show()
+
+            file_name = row[idx - 1].value
+
+            save_path = os.path.join("images", "special_images", f"{file_name}.png")
+
+            image.save(save_path)
 
         print(cel.value)
 
     print(row)  # list of cell values of this row
+
 
 # Loop through the worksheets images
 
@@ -53,6 +63,7 @@ def is_png(image_path):
         return img.format == 'PNG'
     except IOError:
         return False
+
 
 image_path = 'Image.png'
 if is_png(image_path):
@@ -72,6 +83,5 @@ def convert_to_png(image_path, output_path):
     except IOError:
         print("Unable to open the image.")
 
-image_path = 'Image.jpg'
-output_path = 'output_image.png'
-convert_to_png(image_path, output_path)
+
+
